bd289bf21cf0f504a05becf5db4add757d7eda82
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-02-07 17:39:37 -0600

    added dates and stopped the repeat of cities

--- assets/script.js
+++ assets/script.js
@@ -33,31 +33,31 @@ function saveHistory (city){
   let searchHistory = JSON.parse(localStorage.getItem("history"))|| []
   searchHistory.push(city)
   localStorage.setItem( "history" ,JSON.stringify(searchHistory))
+
+ 
 }
 
 
 function display(data) {
-//  console.log(data.main);
- let temp = data.main.temp 
-  // temp = Math.round((temp - 273.15) * 9/5 + 32)
+ console.log(data);
+ let temp0 = data.main.temp 
 //  console.log(temp);
-let humidity = data.main.humidity 
-let wind = data.wind.speed
-let cityName = data.name
-let date = data.dt
+let humidity0 = data.main.humidity 
+let wind0 = data.wind.speed
+let cityName0 = data.name
+let date0 = data.dt //append this to the page 
 // unix time stamp 
-let currentDate = new Date(date * 1000)
+let currentDate = new Date(date0 * 1000)
 let formattedDate = currentDate.toLocaleDateString()
 console.log(formattedDate)
-//appending data to current weather
-//  $("#current-temp").append($("li")("Temp: " + temp))
-$("#city-title").text(" ")
-$("#current-temp").append(`Temp : ${temp} F`);
- $("#current-humidity").append(humidity);
- $("#current-wind").append(wind);
- $("#city-title").append(cityName)
-//  console.log(data);
- 
+
+$("#day-zero").text(" ")
+$("#day-zero").append($("<h4 class='card-title'>").html(cityName0));
+$("#day-zero").append($("<p>").html("Temp: " + temp0 + "  °F"));
+$("#day-zero").append($("<p>").html("Humidity: " + humidity0 + "%"));
+$("#day-zero").append($("<p>").html("Wind Speed: " + wind0+ "  MPH"));
+$("#day-zero").append(formattedDate);
+
 
 }
 //5 day forecast function 
@@ -71,7 +71,7 @@ var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}
       return response.json()
       .then(function(data){
         displayDays(data)
-          console.log(data)
+        
       })
     }else {
       window.alert(response.statusText);
@@ -87,12 +87,16 @@ function displayDays (data) {
 let temp1 = data.list[0].main.temp
 // temp1 = Math.round((temp1 - 273.15) * 9/5 + 32)
 console.log(temp1)
-
+console.log(data);
 
 // day one weather 
 let humidity1 = data.list[0].main.humidity
 let wind1 = data.list[0].wind.speed
 let cityName1 = data.city.name
+let date1 = data.list[0].dt
+let dayOneDate = new Date(date1 * 1000)
+let formattedDate1 = dayOneDate.toLocaleDateString()
+console.log(formattedDate1)
 
 // day one 
 $("#day-one").text(" ")
@@ -100,67 +104,85 @@ $("#day-one").append($("<h4 class='card-title'>").html(cityName1));
 $("#day-one").append($("<p>").html("Temp: " + temp1 + "  °F"));
 $("#day-one").append($("<p>").html("Humidity: " + humidity1 + "%"));
 $("#day-one").append($("<p>").html("Wind Speed: " + wind1+ "  MPH"));
-
+$("#day-one").append(formattedDate1);
 
 //day two weather
-let humidity2 = data.list[1].main.humidity
-let wind2 = data.list[1].wind.speed
+let humidity2 = data.list[8].main.humidity
+let wind2 = data.list[8].wind.speed
 let cityName2 = data.city.name
-let temp2 = data.list[1].main.temp
+let temp2 = data.list[8].main.temp
+let date2 = data.list[8].dt
+let daytwoDate = new Date(date2 * 1000)
+let formattedDate2 = daytwoDate.toLocaleDateString()
 
 //day two 
+$("#day-two").text(" ")
 $("#day-two").append($("<h4>").html(cityName2));
 $("#day-two").append($("<p>").html("Temp: " + temp2 + "  °F"));
 $("#day-two").append($("<p>").html("Humidity: " + humidity2 + "%"));
 $("#day-two").append($("<p>").html("Wind Speed: " + wind2+ "  MPH"));
+$("#day-two").append(formattedDate2);
 
 // day three
-let humidity3 = data.list[2].main.humidity
-let wind3 = data.list[2].wind.speed
+let humidity3 = data.list[16].main.humidity
+let wind3 = data.list[16].wind.speed
 let cityName3 = data.city.name
-let temp3 = data.list[2].main.temp
+let temp3 = data.list[16].main.temp
+let date3 = data.list[16].dt
+let dayThreeDate = new Date(date3 * 1000)
+let formattedDate3 = dayThreeDate.toLocaleDateString()
 
-// day three
+// day three 
+$("#day-three").text(" ")
 $("#day-three").append($("<h4>").html(cityName3));
 $("#day-three").append($("<p>").html("Temp: " + temp3 + "  °F"));
 $("#day-three").append($("<p>").html("Humidity: " + humidity3 + "%"));
 $("#day-three").append($("<p>").html("Wind Speed: " + wind3+ "  MPH"));
+$("#day-three").append(formattedDate3);
 
 // day 4
-let humidity4 = data.list[3].main.humidity
-let wind4 = data.list[3].wind.speed
+let humidity4 = data.list[24].main.humidity
+let wind4 = data.list[24].wind.speed
 let cityName4 = data.city.name
-let temp4 = data.list[3].main.temp
+let temp4 = data.list[24].main.temp
+let date4 = data.list[24].dt
+let dayfourDate = new Date(date4 * 1000)
+let formattedDate4 = dayfourDate.toLocaleDateString()
 
 // day 4
+$("#day-four").text(" ")
 $("#day-four").append($("<h4>").html(cityName4));
 $("#day-four").append($("<p>").html("Temp: " + temp4 + "  °F"));
 $("#day-four").append($("<p>").html("Humidity: " + humidity4 + "%"));
 $("#day-four").append($("<p>").html("Wind Speed: " + wind4+ "  MPH"));
+$("#day-four").append(formattedDate4);
 
 // day 5
-let humidity5 = data.list[4].main.humidity
-let wind5 = data.list[4].wind.speed
+let humidity5 = data.list[32].main.humidity
+let wind5 = data.list[32].wind.speed
 let cityName5 = data.city.name
-let temp5 = data.list[4].main.temp
+let temp5 = data.list[32].main.temp
+let date5 = data.list[32].dt
+let dayFiveDate = new Date(date5 * 1000)
+let formattedDate5 = dayFiveDate.toLocaleDateString()
 
 // day 5
+$("#day-five").text(" ")
 $("#day-five").append($("<h4>").html(cityName5));
 $("#day-five").append($("<p>").html("Temp: " + temp5 + "  °F"));
 $("#day-five").append($("<p>").html("Humidity: " + humidity5 + "%"));
 $("#day-five").append($("<p>").html("Wind Speed: " + wind5+ "  MPH"));
-
+$("#day-five").append(formattedDate5);
 
 
 }
 
 
 
-
-
 button.addEventListener("click" , () => {
   getApi ()
   forecast ()
+  saveHistory ()
 })
 
 
